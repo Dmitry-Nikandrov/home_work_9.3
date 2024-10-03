31b57e773aec51cf5c17e58a329b0b3be1460116
Dmitry <dnikandrov@yandex.ru>
2024-10-03 10:59:09 +0300

    add annotations for functions and check them with poetry

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,8 @@
-def filter_by_state(list_data, state="EXECUTED"):
-    '''принимает список данных и возвращает отфильтрованный список по параметру "state"'''
+from typing import Optional
+
+
+def filter_by_state(list_data: list[dict], state: Optional[str] = "EXECUTED") -> list[dict]:
+    """принимает список данных и возвращает отфильтрованный список по параметру state"""
 
     list_filtered = []
     for i in list_data:
@@ -9,8 +12,8 @@ def filter_by_state(list_data, state="EXECUTED"):
     return list_filtered
 
 
-def sort_by_date(list_data, reverse=True):
+def sort_by_date(list_data: list[dict], rev=True) -> list[dict]:
     """принимает список данных и возвращает отфильтрованный список по убыванию даты"""
 
-    sorted_list = sorted(list_data, key=lambda x: x["date"], reverse=reverse)
+    sorted_list = sorted(list_data, key=lambda x: x["date"], reverse=rev)
     return sorted_list
